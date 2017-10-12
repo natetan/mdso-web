fba6e952c8ce4b77747178f10c0b47851cc28841
Yulong Tan <yulongtan67@gmail.com>
2017-10-12 13:29:16 -0700

    some changes

--- scripts/Controllers/AirlineFormController.js
+++ scripts/Controllers/AirlineFormController.js
@@ -1,7 +1,30 @@
-define(function() {
+define(['Models/AirlineForm', 'Models/AirlineFormField', 'Views/AirlineFormView'], function(AirlineForm, AirlineFormField, AirlineFormView) {
 
-	function displayFields(response, airlineCode) {
-		response = $.parseJSON(response);
+	function displayFields(response) {
+		if (response !== null && response !== 'null') {
+			response = $.parseJSON(response);
+			var latestDocument = result[result.length - 1];
+			var airlines = latestDocument['data'];
+			var milliseconds = latestDocument['_ts'];
+
+
+
+
+
+
+
+
+			$('#option-headers .updated-date-time').html(helpers.getDate(milliseconds));
+			FINAL_JSON_DOCUMENT['data'] = airlines;
+			for (var i = 0; i < airlines.length; i++) {
+				var currentAirline = airlines[i];
+				var airlineCode = currentAirline['AirlineCode'];
+				helpers.setGlobals(airlineCode, currentAirline);
+			}
+			helpers.switchAirlineData('AS');
+		} else {
+			helpers.createAndDisplayModal('noDocsModal', 'No data available', 'No documents found in the database. The default template will be used.');
+			require('ajax').getTemplates();
+		}
 	}
-
 });
--- scripts/Controllers/AirlineMetricsController.js
+++ scripts/Controllers/AirlineMetricsController.js
@@ -1,24 +1,26 @@
 define(['Models/Metric', 'Models/AirlineMetrics','Views/AirlineMetricsView'], function(Metric, AirlineMetrics, AirlineMetricsView) {
 	function displayMetrics(response, airlineCode) {
-		response = $.parseJSON(response);
-		for (var i = 0; i < response.length; i++) {
-			var currentMetric = response[i];
-			var currentAirline = currentMetric['Airline'].trim();
-			var airlineMetricsObject;
-			if (currentAirline === airlineCode) {
-				var metricsArray = [
-					new Metric('A0', currentMetric['A0']),
-					new Metric('A4', currentMetric['A4']),
-		            new Metric('A14', currentMetric['A14']),
-		            new Metric('A30', currentMetric['A30']),
-		            new Metric('D0', currentMetric['D0']),
-		            new Metric('D14', currentMetric['D14']),
-		            new Metric('B0', currentMetric['B0']),
-		            new Metric('B4', currentMetric['B4']),
-		            new Metric('B14', currentMetric['B14'])
-				];
-				airlineMetricsObject = new AirlineMetrics(metricsArray);
-				break;
+		if (response !== null && response !== 'null') {
+			response = $.parseJSON(response);
+			for (var i = 0; i < response.length; i++) {
+				var currentMetric = response[i];
+				var currentAirline = currentMetric['Airline'].trim();
+				var airlineMetricsObject;
+				if (currentAirline === airlineCode) {
+					var metricsArray = [
+						new Metric('A0', currentMetric['A0']),
+						new Metric('A4', currentMetric['A4']),
+			            new Metric('A14', currentMetric['A14']),
+			            new Metric('A30', currentMetric['A30']),
+			            new Metric('D0', currentMetric['D0']),
+			            new Metric('D14', currentMetric['D14']),
+			            new Metric('B0', currentMetric['B0']),
+			            new Metric('B4', currentMetric['B4']),
+			            new Metric('B14', currentMetric['B14'])
+					];
+					airlineMetricsObject = new AirlineMetrics(metricsArray);
+					break;
+				}
 			}
 		}
 		airlineMetricsObject = JSON.parse(airlineMetricsObject);
